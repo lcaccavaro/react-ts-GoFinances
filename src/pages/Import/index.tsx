967902219106971dd4c3389a35c9730e661091c8
Lucas Caccavaro <andradecaccavarolucas@johndeere.com>
2021-01-20 21:39:31 -0300

    Implemented dashboard and import file

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
 
     try {
-      // await api.post('/transactions/import', data);
+      data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
+
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadedFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadedFiles);
   }
 
   return (
